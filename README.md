748d833538a161759123c205a199f5ebaeac9c4f
Silvia Ferrai <silviafer@hotmail.it>
2022-09-24 16:58:31 +0200

    Add tables in hotelmiranda db, add data in tables

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
 Back from the Miranda project, with Node and Express
+
+## MY SQL TABLES:
+
+### USERS
+
+CREATE TABLE users (
+id: INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+full_name: VARCHAR(255) NOT NULL,
+job_title: ENUM('Manager','Room Service','Reception')
+email: VARCHAR(255) NOT NULL,
+photo: VARCHAR(255) NOT NULL,
+phone_number: VARCHAR(20) NOT NULL,
+start_date: DATE NOT NULL,
+working_functions: VARCHAR(255) NOT NULL,
+working_situation: ENUM("ACTIVE", "INACTIVE")
+)
+
+### ROOMS
+
+CREATE TABLE rooms (
+id: INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+photo: VARCHAR(255) NOT NULL,
+room_type: ENUM(('Double Superior','Single Bed','Suite','Double Bed') NOT NULL,
+room_number: INT NOT NULL,
+facilities: VARCHAR(255) NOT NULL,
+price: DECIMAL(10,2) NOT NULL,
+offer_price: DECIMAL(10,2)
+description: VARCHAR(255) NOT NULL,
+status: ENUM('AVAILABLE','BOOKED')
+
+)
+
+### BOOKINGS
+
+CREATE TABLE bookings (
+id: INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+full_name: VARCHAR(255) NOT NULL,
+order_date: DATE,
+check_in: DATE,
+check_out: DATE,
+special_request:VARCHAR(255) ,
+status: ENUM(('In progress','Check In','Check Out')
+idRoom: INT NULL,
+CONSTRAIN id_foreign
+FOREIGN KEY (idRoom) REFERENCES rooms(id)
+ON UPDATE CASCADE
+ON DELETE SET NULL
+)
+
+### CONTACTS
+
+CREATE TABLE contacts (
+id: INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+photo: VARCHAR(255) NOT NULL,
+date: DATE,
+customer: VARCHAR(255) NOT NULL,
+email:VARCHAR(255) NOT NULL,
+phone_number: VARCHAR(20) NOT NULL,
+title: VARCHAR(255) NOT NULL,
+comment: VARCHAR(255) NOT NULL,
+)
--- seed.js
+++ seed.js
@@ -36,11 +36,48 @@ for (let i = 0; i <= 20; i++) {
     faker.helpers.fake("WiFi, Cleaning, Breakfast, AC... "),
     faker.finance.amount(200, 1000, 2),
     faker.finance.amount(200, 500, 2),
-    faker.lorem.paragraph(),
+
     faker.lorem.words(),
     faker.helpers.arrayElement(["AVAILABLE", "BOOKED"]),
   ];
   db.query(sqlRoom, [rooms], function (err, result) {
+    if (err) throw err;
+    else console.dir("Successfully inserted the row in table.");
+    console.log(result);
+  });
+}
+
+let sqlBooking =
+  "INSERT INTO bookings (full_name, order_date, check_in, check_out, special_request, status) VALUES (?)";
+for (let i = 0; i <= 35; i++) {
+  const bookings = [
+    faker.name.fullName(),
+    faker.date.recent(),
+    faker.date.between("2022-10-01T00:00:00.000Z", "2023-03-01T00:00:00.000Z"),
+    faker.date.between("2023-03-01T00:00:00.000Z", "2023-04-22T00:00:00.000Z"),
+    faker.lorem.words(),
+    faker.helpers.arrayElement(["In progress", "Check In", "Check Out"]),
+  ];
+  db.query(sqlBooking, [bookings], function (err, result) {
+    if (err) throw err;
+    else console.dir("Successfully inserted the row in table.");
+    console.log(result);
+  });
+}
+
+let sqlContact =
+  "INSERT INTO contacts (photo, date, customer, email, phone_number, title, comment) VALUES (?)";
+for (let i = 0; i <= 15; i++) {
+  const contacts = [
+    faker.image.avatar(),
+    faker.date.between("2015-10-01T00:00:00.000Z", "2022-07-01T00:00:00.000Z"),
+    faker.name.fullName(),
+    faker.internet.email(),
+    faker.phone.number("+34 ### ## ##"),
+    faker.lorem.words(),
+    faker.lorem.words(50),
+  ];
+  db.query(sqlContact, [contacts], function (err, result) {
     if (err) throw err;
     else console.dir("Successfully inserted the row in table.");
     console.log(result);
