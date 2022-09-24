e19ea96029f3b9d0c3ace98b20f2492b5ea7b38e
Silvia Ferrai <silviafer@hotmail.it>
2022-09-24 18:56:07 +0200

    Add database in bookings controller

--- controllers/bookings-controller.js
+++ controllers/bookings-controller.js
@@ -1,30 +1,78 @@
-const MockReservations = require("../data/mockReservations");
+const { db } = require("../db");
 
 exports.getBookings = (req, res) => {
-  res.json(MockReservations);
+  db.query("SELECT * FROM bookings", (err, rows, field) => {
+    if (!err) res.send(rows);
+    else console.log(err);
+  });
 };
 
 exports.getBooking = (req, res) => {
-  const index = MockReservations.findIndex((item) => item.id === req.params.id);
-  MockReservations[index] = { ...MockReservations[index] };
-  res.json(MockReservations[index]);
+  db.query(
+    "SELECT * FROM bookings WHERE id = ?",
+    [req.params.id],
+    (err, rows, field) => {
+      console.log(req.params.id);
+      if (!err) res.send(rows);
+      else console.log(err);
+    }
+  );
 };
 
 exports.createNewBooking = (req, res) => {
-  const newBooking = { ...req.body };
-  MockReservations.push(newBooking);
-  res.json(newBooking);
+  let booking = req.body;
+  let newBooking = [
+    booking.full_name,
+    booking.order_date,
+    booking.check_in,
+    booking.check_out,
+    booking.special_request,
+    booking.status,
+  ];
+  let sql =
+    "INSERT INTO bookings (full_name, order_date, check_in, check_out, special_request, status) VALUES (?)";
+  db.query(
+    sql,
+    [newBooking],
+
+    (err, rows, fields) => {
+      console.log(rows);
+      if (!err) res.send("Booking created successfully.");
+      else console.log(err);
+    }
+  );
 };
 
 exports.deleteBooking = (req, res) => {
-  let newMockReservations = MockReservations.filter(
-    (item) => item.id !== req.params.id
+  db.query(
+    "DELETE FROM bookings WHERE id = ?",
+    [req.params.id],
+    (err, rows, field) => {
+      if (!err) res.send("Booking deleted successfully.");
+      else console.log(err);
+    }
   );
-  res.json(newMockReservations);
 };
 
 exports.updateBooking = (req, res) => {
-  const index = MockReservations.findIndex((item) => item.id === req.params.id);
-  MockReservations[index] = { ...MockReservations[index], ...req.body };
-  res.json(MockReservations[index]);
+  let booking = req.body;
+  let sql =
+    "UPDATE bookings SET full_name=?, order_date=?, check_in=?, check_out=?, special_request=?, status=? WHERE id =" +
+    req.params.id;
+  db.query(
+    sql,
+    [
+      booking.full_name,
+      booking.order_date,
+      booking.check_in,
+      booking.check_out,
+      booking.special_request,
+      booking.status,
+    ],
+
+    (err, rows, fields) => {
+      if (!err) res.send("Booking updated successfully.");
+      else console.log(err);
+    }
+  );
 };
