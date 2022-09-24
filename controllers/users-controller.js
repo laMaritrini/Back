1d6a2489dde1da54664d4b3d44c8b820945d3f32
Silvia Ferrai <silviafer@hotmail.it>
2022-09-24 19:32:01 +0200

    Add database to users controller

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,28 +1,83 @@
-const MockUsers = require("../data/mockUsers");
+const { db } = require("../db");
 
 exports.getUsers = (req, res) => {
-  res.json(MockUsers);
+  db.query("SELECT * FROM users", (err, rows, field) => {
+    if (!err) res.send(rows);
+    else console.log(err);
+  });
 };
 
 exports.getUser = (req, res) => {
-  const index = MockUsers.findIndex((item) => item.id === req.params.id);
-  MockUsers[index] = { ...MockUsers[index] };
-  res.json(MockUsers[index]);
+  db.query(
+    "SELECT * FROM users WHERE id = ?",
+    [req.params.id],
+    (err, rows, field) => {
+      if (!err) res.send(rows);
+      else console.log(err);
+    }
+  );
 };
 
 exports.createNewUser = (req, res) => {
-  const newUser = { ...req.body };
-  MockUsers.push(newUser);
-  res.json(newUser);
+let user = req.body;
+let newUser = [
+  user.full_name,
+  user.job_title,
+  user.email,
+  user.photo,
+  user.phone_number,
+  user.start_date,
+  user.working_functions,
+  user.working_situation,
+  user.password
+];
+let sql =
+  "INSERT INTO users (full_name, job_title, email, photo, phone_number,start_date,working_functions, working_situation) VALUES (?)";
+db.query(
+  sql,
+  [newUser],
+
+  (err, rows, fields) => {
+    console.log(rows);
+    if (!err) res.send("User created successfully.");
+    else console.log(err);
+  }
+);
 };
 
 exports.deleteUser = (req, res) => {
-  let newMockUsers = MockUsers.filter((item) => item.id !== req.params.id);
-  res.json(newMockUsers);
+  db.query(
+    "DELETE FROM users WHERE id = ?",
+    [req.params.id],
+    (err, rows, field) => {
+      if (!err) res.send("User deleted successfully.");
+      else console.log(err);
+    }
+  );
 };
 
 exports.updateUser = (req, res) => {
-  const index = MockUsers.findIndex((item) => item.id === req.params.id);
-  MockUsers[index] = { ...MockUsers[index], ...req.body };
-  res.json(MockUsers[index]);
+let user = req.body;
+let sql =
+  "UPDATE bookings SET full_name=?, job_title=?, email=?, photo=?, phone_number=?,start_date=?,working_functions=?, working_situation=? WHERE id =" +
+  req.params.id;
+db.query(
+  sql,
+  [
+    user.full_name,
+    user.job_title,
+    user.email,
+    user.photo,
+    user.phone_number,
+    user.start_date,
+    user.working_functions,
+    user.working_situation,
+    user.password,
+  ],
+
+  (err, rows, fields) => {
+    if (!err) res.send("User updated successfully.");
+    else console.log(err);
+  }
+);
 };
